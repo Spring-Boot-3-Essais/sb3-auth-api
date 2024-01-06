3c59c4d4b8e799c04c7c6aaaf108e0a75c425c35
David <david.sirven@libertysurf.fr>
2024-01-06 17:20:26 +0100

    First Commit

---
+++ src/main/java/com/springsecurity/jwt/sb3authapi/Sb3AuthApiApplication.java
@@ -0,0 +1,13 @@
+package com.springsecurity.jwt.sb3authapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb3AuthApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sb3AuthApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springsecurity/jwt/sb3authapi/Sb3AuthApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springsecurity.jwt.sb3authapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb3AuthApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
